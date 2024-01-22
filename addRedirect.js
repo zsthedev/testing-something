497171f3c573e0cbec6a26ed0bb2528b5210ff9c
zsthedev <zsdev166@gmail.com>
2024-01-22 17:51:50 +0500

    Frontend Updated

--- addRedirect.js
+++ addRedirect.js
@@ -1,9 +1,12 @@
-import fs from "fs-extra";
+const fs = require("fs-extra");
 
+// Path to the build folder
 const buildPath = "./build";
 
+// Content of the _redirects file
 const redirectsContent = "/* /index.html 200";
 
+// Write the _redirects file to the build folder
 fs.writeFileSync(`${buildPath}/_redirects`, redirectsContent);
 
 console.log("Added _redirects file to the build folder.");
