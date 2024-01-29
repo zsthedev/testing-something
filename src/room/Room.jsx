aa6a8f5c6d3509defec69f4978adb9ab36742417
zsthedev <zsdev166@gmail.com>
2024-01-29 22:52:11 +0500

    Video Updated

--- src/room/Room.jsx
+++ src/room/Room.jsx
@@ -1,36 +1,41 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
-
+import "./room.scss";
 const Room = () => {
   const { id } = useParams();
   const userId = Date.now().toString();
   const username = "Shahzaib Khan";
+
   const meeting = async (element) => {
-    const appId = 846351004;
-    const serverSecret = "02fc6c52cba41536f7cf28a722f1808e";
-    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
-      appId,
-      serverSecret,
-      id,
-      userId,
-      username
-    );
-    const zc = ZegoUIKitPrebuilt.create(kitToken);
+    try {
+      const appId = 846351004;
+      const serverSecret = "02fc6c52cba41536f7cf28a722f1808e";
+      const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+        appId,
+        serverSecret,
+        id,
+        userId,
+        username
+      );
+      const zc = ZegoUIKitPrebuilt.create(kitToken);
 
-    console.log(kitToken);
+      zc.joinRoom({
+        container: document.getElementById("call-container"),
 
-    zc.joinRoom({
-      container: element,
-
-      scenario: {
-        mode: ZegoUIKitPrebuilt.OneONoneCall,
-      },
-    });
+        scenario: {
+          mode: ZegoUIKitPrebuilt.OneONoneCall,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
+
   return (
     <div
       className="myCallContainer"
+      id="call-container"
       ref={meeting}
       style={{ width: "100vw", height: "100vh" }}
     ></div>
